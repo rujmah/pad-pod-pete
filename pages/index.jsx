7fa5b2dc49df11ecd481cf086831bd32f57c4c85
Robin Mayfield <rjjm@rjjm.net>
2021-03-24 18:29:59 +0000

    initial styles setup

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./index.module.css";
+
+export default function IndexPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.welcome}>Welcome to the shop</div>
+    </div>
+  );
+}
